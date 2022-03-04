26c040a2d0256f7b75c60f657ba8a2070f2e8efb
thesacredcoder <akashsamal4875@gmail.com>
2022-03-05 00:00:29 +0530

    initial commit

---
+++ src/home/Home.jsx
@@ -0,0 +1,15 @@
+import { Navbar } from "../components/navbar/Navbar";
+import "./home.scss";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <Navbar />
+      <img
+        width="100%"
+        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+      />
+    </div>
+  );
+}
